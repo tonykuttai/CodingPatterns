d9a8bdaf4d92a4c557705887867a8946067c62ad
tonykuttai <tonypalampalliyil@gmail.com>
2022-10-26 14:45:01 +0530

    Linked list cycle added

---
+++ FastSlowPointer/LinkedListCycle.cpp
@@ -0,0 +1,39 @@
+// Given the head of a Singly LinkedList, write a function to determine if the LinkedList has a cycle in it or not.
+
+#include <iostream>
+using namespace std;
+
+class ListNode {
+ public:
+  int value = 0;
+  ListNode *next;
+
+  ListNode(int value) {
+    this->value = value;
+    next = nullptr;
+  }
+};
+
+class LinkedListCycle {
+ public:
+  static bool hasCycle(ListNode *head) {
+    // TODO: Write your code here
+    return false;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  ListNode *head = new ListNode(1);
+  head->next = new ListNode(2);
+  head->next->next = new ListNode(3);
+  head->next->next->next = new ListNode(4);
+  head->next->next->next->next = new ListNode(5);
+  head->next->next->next->next->next = new ListNode(6);
+  cout << "LinkedList has cycle: " << LinkedListCycle::hasCycle(head) << endl;
+
+  head->next->next->next->next->next->next = head->next->next;
+  cout << "LinkedList has cycle: " << LinkedListCycle::hasCycle(head) << endl;
+
+  head->next->next->next->next->next->next = head->next->next->next;
+  cout << "LinkedList has cycle: " << LinkedListCycle::hasCycle(head) << endl;
+}
